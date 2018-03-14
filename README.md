17f62e3e65ed5f5a492e6250196e1149aca3dbe2
jeff-the-ma <36683561+jeff-the-ma@users.noreply.github.com>
2018-03-14 14:03:04 -0700

    Create github.py
    Initial creation of file.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Hello World 
+# Hello World
 
-Beep Borp I'm a robot.
+Beep Borp I'm a robot
---
+++ github.py
@@ -0,0 +1,12 @@
+#! usr/bin/env python
+#github.py - A test to see how to Github on Atom
+
+import os, re
+
+
+try:
+    with open('new.csv','w') as f:
+
+    pass
+except Exception as e:
+    raise
