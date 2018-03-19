f7b39303b1888ea62d86d62e9c6d7c5628843a7c
jeff-the-ma <36683561+jeff-the-ma@users.noreply.github.com>
2018-03-19 10:04:25 -0700

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,19 @@
+#! usr/bin/env python
+# Fibonacci.py - A quick introduction to using generators **WOW**
+
+from random import randint as r_Int
+
+def fib():
+    a,b = 0,1
+    while 1:
+        yield a
+        a,b = b, a + b
+
+** BEGIN CODE **
+try:
+    g = fib()
+    for it in range(15):
+        next(g)
+
+except Exception as e:
+    raise
